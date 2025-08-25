952a7e3c9b20fbca03ad2ee56eb270b2e5ecb924
Wthing <wierd8things@gmail.com>
2025-08-25 11:35:43 +0500

    ....

--- views/form/pdf.php
+++ views/form/pdf.php
@@ -185,13 +185,13 @@ $types = [
         padding: 20mm;
     }
 
-    body {
-        font-family: "Times New Roman", serif;
-        font-size: 10pt;
-        line-height: 1.5;
-        margin: 0;
-        padding: 20mm;
-    }
+    /*body {*/
+    /*    font-family: "Times New Roman", serif;*/
+    /*    font-size: 10pt;*/
+    /*    line-height: 1.5;*/
+    /*    margin: 0;*/
+    /*    padding: 20mm;*/
+    /*}*/
 
     .header-right {
         text-align: right;
@@ -246,7 +246,6 @@ $types = [
             padding: 2rem;
             border-radius: 15px;
             box-shadow: 8px 8px 16px #c8c9cc, -8px -8px 16px #ffffff;
-            font-family: "Segoe UI", Arial, sans-serif; /* экранный нормальный шрифт */
         }
 
         .document-view.no-print {
